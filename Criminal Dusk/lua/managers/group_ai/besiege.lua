2b627cecc7655ea356fb16246421b51eca307e54
~/cat/em1/nal/ <emaperry@stupidcat.uk>
2026-07-23 07:59:16 +0100

    updated Winters spawn logic to make more sense

--- Criminal Dusk/lua/managers/group_ai/besiege.lua
+++ Criminal Dusk/lua/managers/group_ai/besiege.lua
@@ -6,29 +6,32 @@ Hooks:OverrideFunction(GroupAIStateBesiege, "phalanx_damage_reduction_enable", f
 Hooks:OverrideFunction(GroupAIStateBesiege, "phalanx_damage_reduction_disable", function() managers.hud:set_buff_enabled("vip", false) end)
 
 Hooks:OverrideFunction(GroupAIStateBesiege, "_check_spawn_phalanx", function(self)
-  self._phalanx_current_spawn_chance = self._phalanx_current_spawn_chance or tweak_data.group_ai.phalanx.spawn_chance.start
-  local now = TimerManager:game():time()
-
-  -- Should we should try spawning Winters?
+  -- Is Winters allowed to spawn?
   local AssaultBegun = self._task_data and self._task_data.assault.active and self._task_data.assault.phase == "build"
-  local IsWintersValid = self._phalanx_center_pos and not self._phalanx_spawn_group and not self._phalanx_despawn_time
-  local SpawnConditionsMet = not self._phalanx_spawn_attempted and self._phalanx_current_spawn_chance > 0
+  local IsPhalanxValid = self._phalanx_center_pos and not self._phalanx_spawn_group and not self._phalanx_despawn_time and not self._phalanx_spawn_attempted
+  local WintersCanSpawn = AssaultBegun and IsPhalanxValid
 
-  local WintersCanSpawn = AssaultBegun and IsWintersValid and SpawnConditionsMet
-  if not WintersCanSpawn then return
+  if not WintersCanSpawn and not self._phalanx_spawn_timer then return
 
-  -- Winters has already been given the go-ahead. Is now the time?
-  elseif self._phalanx_spawn_timer and now >= self._phalanx_spawn_timer then self:_spawn_phalanx() return end
+  elseif self._phalanx_spawn_timer then -- Spawn approved, is it time?
+    local now = TimerManager:game():time()
+    if now >= self._phalanx_spawn_timer then self:_spawn_phalanx() return end
 
-  -- Winters is allowed to spawn but hasn't spawned yet
-  self._phalanx_spawn_attempted = true
-  CrimDusk.Log(FileIdent, "Winters spawn chance: " .. self._phalanx_current_spawn_chance * 100 .. "%")
-  if math.random() > self._phalanx_current_spawn_chance then return end
+  else -- Winters can spawn but has yet to be approved
+    self._phalanx_current_spawn_chance = self._phalanx_current_spawn_chance or tweak_data.group_ai.phalanx.spawn_chance.start
+    if self._phalanx_current_spawn_chance <= 0 then return end
 
-  -- Spawn successful! Give the go-ahead and start a timer
-  CrimDusk.Log(FileIdent, "Winters is spawning!")
-  local BuildDuration = tweak_data.group_ai.besiege.assault.build_duration
-  self._phalanx_spawn_timer = now + math.random(20, BuildDuration)
+    self._phalanx_spawn_attempted = true
+    CrimDusk.Log(FileIdent, "Winters spawn chance: " .. self._phalanx_current_spawn_chance * 100 .. "%")
+
+    if math.random() > self._phalanx_current_spawn_chance then return end
+
+    -- Success! Assign a spawn timer
+    CrimDusk.Log(FileIdent, "Winters is spawning!")
+    local BuildDuration = tweak_data.group_ai.besiege.assault.build_duration
+    local LowerBound = math.max(BuildDuration - 15, 0)
+    self._phalanx_spawn_timer = now + math.random(LowerBound, BuildDuration)
+  end
 end)
 
 Hooks:PreHook(GroupAIStateBesiege, "_end_regroup_task", "CrimDusk_GroupAIAssaultEnd", function(self)
