9bf00fe69fc3dd79ad7b9cdee2928bee338b2813
~/cat/em1/nal/ <emaperry@stupidcat.uk>
2026-08-12 13:30:31 +0100

    Delete Thievenfall/lua/tweakdata directory

--- Thievenfall/lua/tweakdata/assets.lua
+++
@@ -1,6 +0,0 @@
-Hooks:PostHook(AssetsTweakData, "_init_assets", "CrimDusk_InitAssetTweak", function(self)
-  self.grenade_crate.stages = {}
-  self.safe_escape.stages = {}
-  self.ammo_bag.upgrade_lock = { category = "player", upgrade = "additional_assets" }
-  self.health_bag.upgrade_lock = { category = "player", upgrade = "additional_assets" }
-end)
--- Thievenfall/lua/tweakdata/preplanning.lua
+++
@@ -1,4 +0,0 @@
-Hooks:PostHook(PrePlanningTweakData, "init", "CrimDusk_InitPreplanTweak", function(self)
-  self.types.grenade_crate.total = 0
-  self.types.grenade_crate.prio = 0
-end)
