29d34df1c2dad54cd574fe06349d053a0f38da9e
~/cat/em1/nal/ <emaperry@stupidcat.uk>
2026-08-25 12:01:11 +0100

    removed pdth diff name because it doesn't work

--- Thievenfall/lua/peer_sync.lua
+++ Thievenfall/lua/peer_sync.lua
@@ -1,5 +1,5 @@
 if NetworkHelper:IsClient() then return end
 Hooks:PostHook(BaseNetworkSession, "on_peer_sync_complete", "CrimDusk_PeerSync", function(self, _, peer_id)
   if managers.job:current_job_id() == "vit" then NetworkHelper:SendToPeer(peer_id, "CrimDusk_WhiteHousePayout", tweak_data.narrative.jobs.vit.payout[1]) end
-  if Global.CrimDusk.data.heists_won < 5 then NetworkHelper:SendToPeer(peer_id, "CrimDusk_Prologue", true) end
+  --if Global.CrimDusk.data.heists_won < 5 and not CrimDusk.SettingsData.permadeath then NetworkHelper:SendToPeer(peer_id, "CrimDusk_Prologue", true) end
 end)
